297538c5550ecb90b9ade4a28993ba9ba19027e4
Ran Tao <ran@ephexi.com>
2012-07-29 13:24:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grrr::Application.config.secret_token = 'b653bec39f5400032139aeb928aaec9731d57e4dd76fd52b205547336278c3418b40b4106bd2d070e50005b2482c58784089c07f73b62ff83a9de73a1f9f00db'

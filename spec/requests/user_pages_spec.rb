0930e5b14afc123572632fcae2eb7d965a36002f
Ran Tao <ran@ephexi.com>
2012-07-30 19:26:16 -0700

    Finished sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,6 +48,7 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome to GRR experience!!') }
+        it { should have_link('Sign out') }
       end
 
     end
